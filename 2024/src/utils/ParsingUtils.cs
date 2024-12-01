72a1a4110d0c3eb6f8f85f239272da558625b9c2
Will Howcroft <willjhowcroft@gmail.com>
2024-12-01 22:19:48 +0000

    Removed .csproj and .sln from repo and configured GetInput() to suitable path

--- 2024/src/utils/ParsingUtils.cs
+++ 2024/src/utils/ParsingUtils.cs
@@ -5,7 +5,7 @@ namespace _2024.src.utils
         public static string[] GetInput(int day)
         {
             if (day < 1 || day > 25) throw new ArgumentException("Invalid day entered");
-            return File.ReadAllLines($"D:/Programming/advent-of-code/2024/src/inputs/{day}.txt");
+            return File.ReadAllLines($"../2024/src/inputs/{day}.txt");
         }
     }
 }
