efc3948c896bbef57cd65b8d117bc1aa5b89dd8f
Will Howcroft <willjhowcroft@gmail.com>
2024-12-05 20:24:51 +0000

    Day 5 Part 2 solved through brute force

--- 2024/src/Day5.cs
+++ 2024/src/Day5.cs
@@ -14,26 +14,26 @@ namespace _2024.src
 
         private bool IsValidLine(List<int> line)
         {
-            for(int i = 0; i < line.Count; ++i)
+            for (int i = 0; i < line.Count; ++i)
             {
                 int value = line[i];
-                if(forwardRules.TryGetValue(value, out HashSet<int>? forwardRule)) 
+                if (forwardRules.TryGetValue(value, out HashSet<int>? forwardRule))
                 {
-                    foreach(int num in forwardRule)
+                    foreach (int num in forwardRule)
                     {
                         int numIndex = line.IndexOf(num);
-                        if(numIndex == -1) continue;
-                        if(numIndex < i) return false; // If num exists in values, and is mistakenly before query, is invalid
+                        if (numIndex == -1) continue;
+                        if (numIndex < i) return false; // If num exists in values, and is mistakenly before query, is invalid
                     }
                 }
 
-                if(backwardRules.TryGetValue(value, out HashSet<int>? backwardRule))
+                if (backwardRules.TryGetValue(value, out HashSet<int>? backwardRule))
                 {
-                    foreach(int num in backwardRule)
+                    foreach (int num in backwardRule)
                     {
                         int numIndex = line.IndexOf(num);
-                        if(numIndex == -1) continue;
-                        if(numIndex > i) return false; // If num exists in values, and is mistakenly before query, is invalid
+                        if (numIndex == -1) continue;
+                        if (numIndex > i) return false; // If num exists in values, and is mistakenly before query, is invalid
                     }
                 }
 
@@ -45,10 +45,10 @@ namespace _2024.src
         private int GetLineTotals()
         {
             int total = 0;
-            foreach(List<int> value in values)
+            foreach (List<int> value in values)
             {
-                if(value.Count == 0) continue;
-                if(IsValidLine(value)) total += value[value.Count / 2];
+                if (value.Count == 0) continue;
+                if (IsValidLine(value)) total += value[value.Count / 2];
             }
             return total;
         }
@@ -56,40 +56,56 @@ namespace _2024.src
         private List<List<int>> GetInvalidLines()
         {
             List<List<int>> invalidLines = [];
-            foreach(List<int> value in values)
+            foreach (List<int> value in values)
             {
-                if(value.Count == 0) continue;
-                if(!IsValidLine(value)) invalidLines.Add(value);
+                if (value.Count == 0) continue;
+                if (!IsValidLine(value)) invalidLines.Add(value);
             }
 
             return invalidLines;
         }
-        
+
         private void FixInvalidLine(List<int> line)
         {
-            for(int p = 0; p < line.Count; ++p)
+            int len = line.Count;
+            while (!IsValidLine(line))
             {
-                int pivot = line[p];
-                HashSet<int> forwardRule = forwardRules[pivot];
-                HashSet<int> backwardRule = backwardRules[pivot];
-
-                if(forwardRule.Count == 0 && backwardRule.Count == 0) continue;
-
-                //TODO Sort potential bounds errors
-                for(int i = 0; i < line.Count; ++i)
+                for (int p = 0; p < len; ++p)
                 {
-                    int num = line[i];
-                    if(i == p) continue;
-                    // If the num needs to be moved after or before the current pivot, delete it from its old pos and insert it into its new position
-                    if(p != line.Count - 1 && i < p && forwardRule.Contains(num)) 
+                    int pivot = line[p];
+
+                    bool forwardRulesExist = forwardRules.TryGetValue(pivot, out HashSet<int>? forwardRule);
+                    bool backwardRulesExist = backwardRules.TryGetValue(pivot, out HashSet<int>? backwardRule);
+
+                    // If pivot doesn't have any rules associated with it and doesn't care where it is, can continue
+                    if (!forwardRulesExist && !backwardRulesExist) continue;
+
+
+                    // For all numbers that are before the pivot that shouldn't be, insert them after
+                    if (forwardRulesExist)
                     {
-                        line.Insert(p + 1, num);
-                        line.RemoveAt(i);
+                        for (int i = 0; i < p; ++i)
+                        {
+                            int num = line[i];
+                            if (!forwardRule!.Contains(num)) continue;
+
+                            if (p != len - 1) line.Insert(p + 1, num); // Cant insert at end so need to add
+                            else line.Add(num);
+
+                            line.RemoveAt(i);
+                        }
                     }
-                    if(p != 0 && i > p && backwardRule.Contains(num))
+
+                    // For all numbers that are after the pivot that shouldn't be, insert them before
+                    if (backwardRulesExist)
                     {
-                        line.Insert(p - 1, num);
-                        line.RemoveAt(i + 1);
+                        for (int i = p + 1; i < len; ++i)
+                        {
+                            int num = line[i];
+                            if (!backwardRule!.Contains(num)) continue;
+                            line.Insert(p, num);
+                            line.RemoveAt(i + 1);
+                        }
                     }
                 }
             }
@@ -98,35 +114,58 @@ namespace _2024.src
 
         public string? ExecPartA()
         {
-            return GetLineTotals().ToString();
+            int total = 0;
+            foreach (List<int> value in values)
+            {
+                if (value.Count == 0) continue;
+                if (IsValidLine(value)) total += value[value.Count / 2];
+            }
+            return total.ToString();
         }
 
         public string? ExecPartB()
         {
-            return null;
+            int total = 0;
+            List<List<int>> invalidLines = GetInvalidLines();
+            foreach (List<int> invalidLine in invalidLines)
+            {
+                FixInvalidLine(invalidLine);
+                total += invalidLine[invalidLine.Count / 2];
+            }
+
+            return total.ToString();
         }
 
         public void Setup(string[] input, string continuousInput)
         {
-            string[] parts = continuousInput.Split("\n\n");
-            if(parts.Length != 2) return;
+            string normalizedInput = continuousInput.Replace("\r\n", "\n");
+            string[] parts = normalizedInput.Split("\n\n");
+            if (parts.Length != 2) return;
 
             string[] ruleStrings = parts[0].Split('\n');
             string[] valueStrings = parts[1].Split('\n');
 
-            foreach(string rule in ruleStrings)
+            foreach (string rule in ruleStrings)
             {
                 string[] split = rule.Split('|');
-                if(split.Length != 2) return;
-                
+                if (split.Length != 2) return;
+
                 int first = int.Parse(split[0]);
                 int last = int.Parse(split[1]);
 
-                if(forwardRules.TryGetValue(first, out HashSet<int>? forwardKeys)) forwardKeys.Add(last);
-                else forwardRules.Add(first, new(last));
+                if (forwardRules.TryGetValue(first, out HashSet<int>? forwardKeys)) forwardKeys.Add(last);
+                else
+                {
+                    forwardRules.Add(first, []);
+                    forwardRules[first].Add(last);
+                }
 
-                if(backwardRules.TryGetValue(last, out HashSet<int>? backwardKeys)) backwardKeys.Add(first);
-                else backwardRules.Add(last, new(first));
+                if (backwardRules.TryGetValue(last, out HashSet<int>? backwardKeys)) backwardKeys.Add(first);
+                else
+                {
+                    backwardRules.Add(last, []);
+                    backwardRules[last].Add(first);
+                }
             }
 
             values = ParsingUtils.ParseDigitsList(valueStrings);
