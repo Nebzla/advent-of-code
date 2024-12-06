7245b8ddd1fca0a59023225aa31f02260b296ba8
Will Howcroft <willjhowcroft@gmail.com>
2024-12-06 18:25:43 +0000

    Day 6 Part 2 Complete

---
+++ 2024/src/Day7.cs
@@ -0,0 +1,27 @@
+using _2024.src.Interfaces;
+using _2024.src.utils;
+
+namespace _2024.src
+{
+    public class Day7 : ISolution
+    {
+        public ushort DayNumber => 67;
+
+
+        public string? ExecPartA()
+        {
+            return null;
+        }
+
+        public string? ExecPartB()
+        {
+            return null;
+        }
+
+        public void Setup(string[] input, string continuousInput)
+        {
+            return;
+        }
+
+    }
+}
